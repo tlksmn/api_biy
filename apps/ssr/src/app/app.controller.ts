c6fc8fc232daa793b17561df6bc6681c9a0235c8
Yaahh <111268404+tlksmn@users.noreply.github.com>
2023-05-15 01:52:50 +0600

    robots.txt && sitemap

--- apps/ssr/src/app/app.controller.ts
+++ apps/ssr/src/app/app.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 import { AppService } from './app.service';
+import * as process from 'process';
 
 @Controller()
 export class AppController {
@@ -10,5 +14,16 @@ export class AppController {
   @Render('index')
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('robots.txt')
+  getRobotsIndex(@Res() response: Response) {
+    const file = createReadStream(join(process.cwd() + '/robots.txt'));
+    file.pipe(response);
+  }
+  @Get('sitemap_index.xml')
+  getSiteMapIndex(@Res() response: Response) {
+    const file = createReadStream(join(process.cwd() + '/sitemap.xml'));
+    file.pipe(response);
   }
 }

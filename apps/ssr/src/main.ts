2a12274747d9355f6d28a7e1489de7f50855b61a
Yaahh <111268404+tlksmn@users.noreply.github.com>
2023-05-15 02:16:51 +0600

    helmet for header

--- apps/ssr/src/main.ts
+++ apps/ssr/src/main.ts
@@ -10,9 +10,11 @@ import { AppModule } from './app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'path';
 import * as express from 'express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(helmet());
   app.set('views', path.join(__dirname, 'view'));
   app.set('view engine', 'pug');
   app.use('/assets', express.static(path.join(__dirname, 'assets')));

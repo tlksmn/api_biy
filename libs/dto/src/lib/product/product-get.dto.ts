3d623a9a2d4b77c4c19677510227424e4b55ccd3
Yaahh <111268404+tlksmn@users.noreply.github.com>
2023-06-04 12:49:51 +0500

    readmemd file edit

--- libs/dto/src/lib/product/product-get.dto.ts
+++ libs/dto/src/lib/product/product-get.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsOptional, Max, Min } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class ProductGetDto {
